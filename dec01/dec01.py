2348d713ee8c7f8612a89c26e48775d8394468da
Yuvika Suvarna <yuvika.suvarna@gmail.com>
2022-12-01 22:50:15 +0000

    First gold star

---
+++ dec01/dec01.py
@@ -0,0 +1,19 @@
+
+
+# advent of code
+elf_calorie = {}
+elf = 0
+def parse_input(line):
+    global elf_calorie, elf
+    if line.strip() == "":
+        elf += 1
+    else:
+        calorie = elf_calorie.get(elf, 0) + int(line.strip())
+        elf_calorie[elf] = calorie
+
+
+if __name__ == "__main__":
+    for line in open("puzzle.txt"):
+        parse_input(line)
+
+    print(max(elf_calorie.values()))
